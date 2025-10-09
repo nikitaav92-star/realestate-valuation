ec7c3fdddab4e9da33a461f3a3ba93d2109c84f2
root <root@bizarresystem.aeza.network>
2025-10-09 12:13:46 +0000

    fix: correct models with default strings

--- etl/models.py
+++ etl/models.py
@@ -2,14 +2,14 @@ from pydantic import BaseModel
 
 class Listing(BaseModel):
     id: int
-    url: str = 
+    url: str = ""
     region: int = 0
-    deal_type: str = 
+    deal_type: str = ""
     rooms: int = 0
     area_total: float = 0.0
     floor: int = 0
-    address: str = 
-    seller_type: str = 
+    address: str = ""
+    seller_type: str = ""
 
 class PricePoint(BaseModel):
     id: int
